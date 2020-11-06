135e140dd6e1e90ea9a09f61789d99836991e321
bkunjesh <bkunjesh@gmail.com>
2020-11-06 12:45:40 +0530

    Codeforces Round #654 (Div. 2)

---
+++ competitive code/C_A_Cookie_for_You.cpp
@@ -0,0 +1,45 @@
+//@CodesUp
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
+#define f(i, k) for (int i = 0; i < k; i++)
+#define F first
+#define dbg(x) cout << #x << " " << x << endl;
+#define S second
+#define endl "\n"
+#define rep(i, n) for (int i = 1; i <= n; i++)
+#define rew(i, a, b) for (int i = a; i <= b; i++)
+#define mod 1000000007
+const int inf = 1e18;
+int N = 200005;
+
+int n;
+
+void solve()
+{
+    int i, j, k;
+    int m, a, b;
+    cin >> a >> b >> n >> m;
+    if (a + b >= n + m && m <= min(a, b))
+        cout << "Yes" << endl;
+    else
+    {
+        cout << "No" << endl;
+    }
+
+    return;
+}
+
+signed main()
+{
+    fast int t = 1, i, j, k;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
