0e078d1cfd17b7a75a3cb6ce1b12f82b611670ea
bkunjesh <bkunjesh@gmail.com>
2020-11-06 15:23:12 +0530

    Codeforces Round #654 (Div. 2)

---
+++ competitive code/B_Magical_Calendar.cpp
@@ -0,0 +1,49 @@
+//@CodesUp
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);
+#define f(i,k) for(int i=0;i<k;i++)
+#define F first
+#define dbg(x)  cout<<#x<<" "<<x<<endl;
+#define S second
+#define endl "\n"
+#define rep(i,n) for(int i=1;i<=n;i++)
+#define rew(i,a,b) for(int i=a;i<=b;i++)
+#define mod 1000000007
+const int inf = 1e18;
+int N=200005;
+
+int n;
+
+void solve()
+{
+    int i, j, k,r;
+
+    cin >> n >> r;
+    if(n>r)
+    {
+        cout << (r + 1) * r / 2 << endl;
+        return;
+    }
+    else
+    {
+        cout << (n - 1) * n / 2 +1 << endl;
+    }
+    
+    
+
+    return;
+}
+
+signed main()
+{
+    fast
+    int t = 1, i, j, k;
+    cin >> t;
+    while (t--)
+    {
+        solve();		
+    }
+return 0;
+}
